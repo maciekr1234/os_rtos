36fe919b29d88c6b3b2d63df602a0a8bd42830bb
maciekr1234 <23438866+maciekr1234@users.noreply.github.com>
2020-11-30 19:48:44 +0000

    update

--- tools/pio_build.py
+++ tools/pio_build.py
@@ -13,11 +13,17 @@ global_env = DefaultEnvironment()
 
 
 # pass flags to a global build environment (for all libraries, etc)
-
-RTOS_INCLUDE_DIR = realpath(join("..", "include", "rtos"))
-FREE_RTOS_PLUS_POSIX_PATH = realpath(join("..", "lib", "FreeRTOS-Plus-POSIX"))
+INCLUDE_DIR = realpath(join("..","include"))
+SRC_DIR = realpath(join("..","src"))
+RTOS_INCLUDE_DIR = join(INCLUDE_DIR, "rtos")
 
 global_env.Append(
+    BUILD_UNFLAGS=[
+        "-std=gnu++14", "-std=gnu11",
+    ],
+    BUILD_FLAGS=[
+        "-std=gnu++2a", "-std=gnu17",
+    ],
     CPPDEFINES=[
 
         # ("OS_RTOS_CMSIS1", 0),
@@ -25,20 +31,13 @@ global_env.Append(
 
     ],
     LINKFLAGS=[
-
+            "-Wl,--wrap=malloc",
+            "-Wl,--wrap=_malloc_r"
     ]
 
 )
 
 env.Append(CPPPATH=[
-    # join(FREE_RTOS_PLUS_POSIX_PATH, "include"),
-    # join(FREE_RTOS_PLUS_POSIX_PATH, "include", "private"),
-    # join(FREE_RTOS_PLUS_POSIX_PATH, "FreeRTOS-Plus-POSIX", "include"),
-    # join(FREE_RTOS_PLUS_POSIX_PATH, "FreeRTOS-Plus-POSIX", "source"),
-    # join(FREE_RTOS_PLUS_POSIX_PATH, "include", "FreeRTOS_POSIX"),
-    #
-    # include/rtos:
-    #
-    # join(RTOS_INCLUDE_DIR, "detail"),
-    # join(RTOS_INCLUDE_DIR, "detail", "bits"),
+
+
 ])
